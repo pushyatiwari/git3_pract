b618d3aefe17c57b3ac6716e7e1829b08ab52757
PushyaTiwari30 <36076546+pushyatiwari@users.noreply.github.com>
2021-02-06 12:14:33 +0530

    s1 commit

---
+++ s1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir files
+cd files
+for i in {1..5}
+do 
+  touch $i.txt
+done
+
+echo file1Content >> 1.txt
+
+echo enter file name to copy to
+read filen1
+
+cp 1.txt $filen1
+
+echo enter filename to write to
+read filen2
+echo write contents
+read contents
+
+echo $contents >> $filen2
+echo reading 1st file that is copied
+cat $filen1
+echo reading 2nd file
+cat $filen2
+echo end
+
